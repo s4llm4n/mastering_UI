172025dbff14ed0826b33843127980df713e96e5
s4llm4n <s4llm4n@gmail.com>
2023-06-14 10:44:58 +0700

    gojek part 2

--- lib/gojek.dart
+++ lib/gojek.dart
@@ -30,6 +30,7 @@ class Gojek extends StatelessWidget {
 
   Widget itemFood() {
     return Container(
+      width: 120,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
           color: Colors.white,
@@ -44,13 +45,13 @@ class Gojek extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: Image.network(
-                "https://rasabunda.com/wp-content/uploads/2022/06/Nasi-Goreng-Terasi-Belacan.jpg",
-                width: 120,
-                height: 80,
-                fit: BoxFit.cover,
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.network(
+                  "https://cdn1-production-images-kly.akamaized.net/a6xvLhjIWqlrNOdjaI40qxT_Jr0=/0x148:1920x1230/640x360/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3093328/original/069244600_1585909700-fried-2509089_1920.jpg",
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
             Padding(
