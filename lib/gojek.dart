f4c496e8acafeba0acec4b06c148782c13aaa091
s4llm4n <s4llm4n@gmail.com>
2023-06-14 11:16:59 +0700

    gojek finished

--- lib/gojek.dart
+++ lib/gojek.dart
@@ -8,7 +8,7 @@ class Gojek extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Gojek')),
       body: SizedBox(
-        height: 180,
+        height: 280,
         child: ListView.builder(
           scrollDirection: Axis.horizontal,
           itemCount: 8,
@@ -30,11 +30,11 @@ class Gojek extends StatelessWidget {
 
   Widget itemFood() {
     return Container(
-      width: 120,
+      width: 220,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
           color: Colors.white,
-          boxShadow: [
+          boxShadow: const [
             BoxShadow(
               blurRadius: 6,
               color: Colors.black26,
@@ -46,28 +46,60 @@ class Gojek extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Expanded(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Image.network(
-                  "https://cdn1-production-images-kly.akamaized.net/a6xvLhjIWqlrNOdjaI40qxT_Jr0=/0x148:1920x1230/640x360/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3093328/original/069244600_1585909700-fried-2509089_1920.jpg",
-                  fit: BoxFit.cover,
-                ),
+              child: Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.network(
+                      "https://cdn1-production-images-kly.akamaized.net/a6xvLhjIWqlrNOdjaI40qxT_Jr0=/0x148:1920x1230/640x360/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3093328/original/069244600_1585909700-fried-2509089_1920.jpg",
+                      fit: BoxFit.cover,
+                      width: 220,
+                    ),
+                  ),
+                  Positioned(
+                    top: 16,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(30),
+                          bottomRight: Radius.circular(30),
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                      child: const Text (
+                        '30% off', 
+                      style: TextStyle(color: Colors.white),),
+                    ),
+                  ),
+                ],
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8),
+              padding: const EdgeInsets.all(16),
               child: Column(
-                children: [
-                  Text(
-                '2.54 Km',
-                style: TextStyle(color: Colors.grey[600]),
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [  
+                Text(
+                  '2.54 Km',
+                  style: TextStyle(color: Colors.grey[600]),
+                ),
+                const SizedBox(height: 8,),
+                const Text(
+                'Nasi Goreng Ganteng, Tarogong Kaler',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
               ),
-              const Text(
-                'Netflix',
-                style: TextStyle(fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(height: 4,),
-              const Text('18 MB'),
+                const SizedBox(height: 8,),
+                Row(
+                  children: [
+                    Icon(Icons.star, color: Colors.orange[800], size: 20,),
+                    const SizedBox(width: 8,),
+                    const Text('4.3'),
+                    const Text(' * 100+ Rating'),
+                  ],
+                ),
                 ],
               ),
             ),
